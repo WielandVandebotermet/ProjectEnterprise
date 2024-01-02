1d3eca75dca7139466daaac2e09c77ab82e5ea8a
Wieland Vandebotermet <Wieland@live.be>
2024-01-02 17:20:08 +0100

    update

--- koers/src/main/java/fact/it/koers/repository/KoersRepository.java
+++ koers/src/main/java/fact/it/koers/repository/KoersRepository.java
@@ -5,10 +5,8 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 import org.springframework.transaction.annotation.Transactional;
 
-import java.util.List;
-
 @Repository
 @Transactional
 public interface KoersRepository extends JpaRepository<Koers, Long> {
-    List<Koers> findByName(List<String> Name);
+
 }
--- wielrennen/src/main/java/fact/it/wielrennen/repository/WielrennerRepository.java
+++ wielrennen/src/main/java/fact/it/wielrennen/repository/WielrennerRepository.java
@@ -5,10 +5,7 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 import org.springframework.transaction.annotation.Transactional;
 
-import java.util.List;
-
 @Repository
 @Transactional
 public interface WielrennerRepository extends JpaRepository<Wielrenner, Long> {
-    List<Wielrenner> findByGroupCode(List<String> GroupCode);
 }
